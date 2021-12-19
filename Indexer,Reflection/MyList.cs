5be1b3d693574c44c5ef1e047d2e3c3a2f631258
Rashad Gasimzada <rashad.gasimzade.2000@gmail.com>
2021-12-19 22:48:17 +0400

    MyList with Add method created

---
+++ Indexer,Reflection/MyList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexer_Reflection
+{
+    public class MyList<T>
+    {
+        public MyList()
+        {
+            list = new T[10];
+        }
+        public T[] list;
+        public static int count;
+        public void Add(T value)
+        {
+            if (count != 0 && count % 10 == 0)
+            {
+                Array.Resize(ref list, list.Length + 10);
+            }
+            list[count] = value;
+            count++;
+        }
+    }
+}
---
+++ Indexer,Reflection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> vs = new List<int>();
+            vs.Add(5);
+            foreach (var item in vs)
+            {
+
+            }
+        }
+    }
+}
